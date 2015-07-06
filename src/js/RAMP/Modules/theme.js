c5cd81ab3f45f89d8b14279f60e9d3fe331f4cee
Spencer Wahl <spencer.wahl@ec.gc.ca>
2015-07-06 10:44:57 -0400

    refactor(core): jscs and jshint refactors
    Theme.js is now following the new jscs and jshint rules.
    
    Relates #10384

--- src/js/RAMP/Modules/theme.js
+++ src/js/RAMP/Modules/theme.js
@@ -1,7 +1,8 @@
 ﻿/*global define, TimelineLite, TweenLite, $ */
 
 /**
-* This submodule contains theme-specific classes with animation sequences such as Full Screen transition or tooltip setter helper method.
+* This submodule contains theme-specific classes with animation sequences such as Full Screen transition or tooltip
+* setter helper method.
 *
 * @module RAMP
 * @submodule Theme
@@ -15,24 +16,25 @@
 * @uses Util
 */
 
-define(["utils/util", "utils/popupManager"],
+define(['utils/util', 'utils/popupManager'],
     function (UtilMisc, PopupManager) {
-        "use strict";
+        'use strict';
 
-        var body = $("body"),
-            wbCore = $("main"),
-            wbFoot = $("footer"),
-            footContainer = wbFoot.find('.footer-container'),
-            
-            megaMenuDiv = $("#wb-sm"),
-            //navigation = $("#wb-bar"),
-            //title = navigation.next(),
+        var body = $('body');
+        var wbCore = $('main');
+        var wbFoot = $('footer');
+        var footContainer = wbFoot.find('.footer-container');
 
-            header = $("body>header"),
+        var megaMenuDiv = $('#wb-sm');
 
-            transitionDuration = 0.5,
+        //var navigation = $('#wb-bar');
+        //var title = navigation.next();
 
-            layout = {
+        var header = $('body>header');
+
+        var transitionDuration = 0.5;
+
+        var layout = {
                 headerHeight: 194,
                 headerHeightCollapsed: 56,
 
@@ -41,38 +43,45 @@ define(["utils/util", "utils/popupManager"],
 
                 //subtitleHeight: 35,
 
-                toolbarHeight: 32
-            },
+                toolbarHeight: 32,
+            };
 
-            // height gain from the fullscreening the template
-            heightGain = layout.headerHeight - layout.headerHeightCollapsed + layout.footerHeight - layout.footerHeightCollapsed,
+        // height gain from the fullscreening the template
+        var heightGain = layout.headerHeight - layout.headerHeightCollapsed +
+                         layout.footerHeight - layout.footerHeightCollapsed;
 
-            footerTimeLine = new TimelineLite(
+        var footerTimeLine = new TimelineLite(
                 {
-                    paused: true
-                }),
+                    paused: true,
+                });
 
-            isFullScreen = false,
-            fullScreenTimeLine = new TimelineLite({ paused: true }),
-            subpanelTimeline = new TimelineLite();
+        var isFullScreen = false;
+        var fullScreenTimeLine = new TimelineLite({ paused: true });
+        var subpanelTimeline = new TimelineLite();
 
         footerTimeLine
-            .to(footContainer, transitionDuration, { height: '471px', ease: "easeOutCirc" })
-            .set(footContainer, { scrollTop: 0 }, 0) // this is needed to reset scroll when you tab to links in the footer nav
-            .set(wbFoot, { className: "+=expanded" }, 0);
+            .to(footContainer, transitionDuration, { height: '471px', ease: 'easeOutCirc' })
+
+            // this is needed to reset scroll when you tab to links in the footer nav
+            .set(footContainer, { scrollTop: 0 }, 0)
+            .set(wbFoot, { className: '+=expanded' }, 0);
 
         // tweening wet template parts
         fullScreenTimeLine
-            .to(header, transitionDuration, { top: "-99px", position: "relative", ease: "easeOutCirc" }, 0)
-            .set([megaMenuDiv], { display: "none !important" })
+            .to(header, transitionDuration, { top: '-99px', position: 'relative', ease: 'easeOutCirc' }, 0)
+            .set([megaMenuDiv], { display: 'none !important' })
 
-            //.to(title, transitionDuration, { top: "-22px" }, 0)
+            //.to(title, transitionDuration, { top: '-22px' }, 0)
 
-            //.fromTo(body, transitionDuration, { "background-position-y": "43px" }, { "background-position-y": "-26px", ease: "easeOutCirc" }, 0)
-            .to(wbCore, transitionDuration, { top: layout.headerHeightCollapsed, bottom: layout.footerHeightCollapsed, ease: "easeOutCirc" }, 0)
-            .to(wbFoot, transitionDuration, { height: layout.footerHeightCollapsed, ease: "easeOutCirc" }, 0)
+            //.fromTo(body, transitionDuration, { 'background-position-y': '43px' },
+            //    { 'background-position-y': '-26px', ease: 'easeOutCirc' }, 0)
+            .to(wbCore, transitionDuration,
+                { top: layout.headerHeightCollapsed, bottom: layout.footerHeightCollapsed, ease: 'easeOutCirc' }, 0)
+            .to(wbFoot, transitionDuration, { height: layout.footerHeightCollapsed, ease: 'easeOutCirc' }, 0)
 
-            .call(function () { body.addClass("full-screen"); }) // set full-screen class here, not in the callback since callbacks can be overwritten by fullScreenCallback function
+            // set full-screen class here, not in the callback since callbacks can be overwritten
+            // by fullScreenCallback function
+            .call(function () { body.addClass('full-screen'); })
 
             .add(subpanelTimeline, 0); // special timeline to tween subpanels
 
@@ -85,59 +94,82 @@ define(["utils/util", "utils/popupManager"],
         function _toggleFullScreenMode(fullscreen) {
             subpanelTimeline
                 .clear() // need to recreate this timeline every time to capture newly created subpanels
-                .fromTo(".sub-panel-container.summary-data-details", transitionDuration,
-                    { top: layout.headerHeight + layout.toolbarHeight, bottom: layout.footerHeight },
-                    { top: layout.headerHeightCollapsed + layout.toolbarHeight, bottom: layout.footerHeightCollapsed, ease: "easeOutCirc" }, 0)
-                .fromTo(".sub-panel-container.full-data-details", transitionDuration,
-                    { top: layout.headerHeight, bottom: layout.footerHeight },
-                    { top: layout.headerHeightCollapsed, bottom: layout.footerHeightCollapsed, ease: "easeOutCirc" }, 0);
+                .fromTo('.sub-panel-container.summary-data-details', transitionDuration,
+                    {
+                        top: layout.headerHeight + layout.toolbarHeight,
+                        bottom: layout.footerHeight,
+                    },
+                    {
+                        top: layout.headerHeightCollapsed + layout.toolbarHeight,
+                        bottom: layout.footerHeightCollapsed,
+                        ease: 'easeOutCirc',
+                    }, 0)
+                .fromTo('.sub-panel-container.full-data-details', transitionDuration,
+                    {
+                        top: layout.headerHeight,
+                        bottom: layout.footerHeight,
+                    },
+                    {
+                        top: layout.headerHeightCollapsed,
+                        bottom: layout.footerHeightCollapsed,
+                        ease: 'easeOutCirc',
+                    }, 0);
 
             isFullScreen = UtilMisc.isUndefined(fullscreen) ? !isFullScreen : fullscreen;
 
             if (isFullScreen) {
-                // need to tween datatables separately since it's very cumbersome to calculate their exact height - easier just to adjust height up/down by height gained when fullscreening the template
+                // need to tween datatables separately since it's very cumbersome to calculate their exact height -
+                // easier just to adjust height up/down by height gained when fullscreening the template
                 TweenLite
-                    .to(".full-data-mode .dataTables_scrollBody", transitionDuration,
-                        { height: "+=" + heightGain, ease: "easeOutCirc", delay: 0.02 }); // animate height of the datatable scrollBody since it's explicitly set ,
+                    .to('.full-data-mode .dataTables_scrollBody', transitionDuration,
+
+                        // animate height of the datatable scrollBody since it's explicitly set ,
+                        { height: '+=' + heightGain, ease: 'easeOutCirc', delay: 0.02 });
 
                 fullScreenTimeLine.play();
 
             } else {
                 TweenLite
-                    .to(".full-data-mode .dataTables_scrollBody", transitionDuration - 0.02,
-                        { height: "-=" + heightGain, ease: "easeInCirc" }); // animate height of the datatable scrollBody since it's explicitly set ,
+                    .to('.full-data-mode .dataTables_scrollBody', transitionDuration - 0.02,
 
-                body.removeClass("full-screen");
+                        // animate height of the datatable scrollBody since it's explicitly set ,
+                        { height: '-=' + heightGain, ease: 'easeInCirc' });
+
+                body.removeClass('full-screen');
                 fullScreenTimeLine.reverse();
             }
         }
 
         // register a popup to open footer navigation on hover and focus
-        PopupManager.registerPopup(wbFoot, "hoverIntent, focus",
+        PopupManager.registerPopup(wbFoot, 'hoverIntent, focus',
             function (d) {
                 footerTimeLine.eventCallback('onComplete',
                     function () {
                         d.resolve();
                     });
+
                 footerTimeLine.play();
             },
+
             {
-                targetSelector: ".wb-navcurr, .wb-navcurr a",
+                targetSelector: '.wb-navcurr, .wb-navcurr a',
                 useAria: false,
                 timeout: 500,
                 closeHandler: function (d) {
                     footerTimeLine.eventCallback('onReverseComplete',
                         function () {
-                            footContainer.scrollTop(0); // this is needed to reset scroll when you tab to links in the footer nav
+                            // this is needed to reset scroll when you tab to links in the footer nav
+                            footContainer.scrollTop(0);
                             d.resolve();
                         });
+
                     footerTimeLine.reverse();
-                }
+                },
             }
         );
 
         // hide footer navigation on load
-        TweenLite.to(footContainer, transitionDuration, { height: "84px", ease: "easeOutCirc" });
+        TweenLite.to(footContainer, transitionDuration, { height: '84px', ease: 'easeOutCirc' });
 
         return {
             /**
@@ -177,7 +209,8 @@ define(["utils/util", "utils/popupManager"],
                 _toggleFullScreenMode(fullscreen);
 
                 return this;
-            }
+            },
         };
     }
+
 );
